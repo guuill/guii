286a343c6378ac5c373b4d790c218a55c8ff1633
guilherme_gustavo <guilherme_gustavo@10.1.48.192>
2013-08-02 14:52:53 -0300

    Criando a classe MarcaTableModel.

--- CrudJdbc/src/crudjdbc/MarcaTableModel.java
+++ CrudJdbc/src/crudjdbc/MarcaTableModel.java
@@ -4,6 +4,8 @@
  */
 package crudjdbc;
 
+import br.com.senai.negocio.Marca;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
@@ -14,10 +16,31 @@ import javax.swing.table.AbstractTableModel;
 public class MarcaTableModel extends AbstractTableModel {
     private static final int COL_ID = 0;
     private static final int COL_NOME = 1;
-   
+    
     private String[] colunas = {"id","Nome",};
     
     private List<Marca>linhas;
+    
+    
+    
+    public MarcaTableModel(){
+        linhas = new ArrayList<Marca>();
+                
+    }
+    public MarcaTableModel(List<Marca> marcas){
+      linhas = new ArrayList<Marca>();  
+    }
+    public String getColumnName(int columnIndex){
+        return colunas [columnIndex]; 
+    }
+    public Class getColumnClass (int columnindex){
+        if (columnindex == COL_ID){
+            return Integer.class;
+        }else if (columnindex == COL_NOME){
+            return String.class;
+        }
+        return null;
+    }
     
     //retorna a qtde de linhas da tabela
     @Override
@@ -32,13 +55,53 @@ public class MarcaTableModel extends AbstractTableModel {
     //retorna o dados do objeto da linha
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-    Marca m = linhas.get(row);
+    Marca m = linhas.get(rowIndex);
     if (columnIndex == COL_ID){
         return m.getId();
         
-    }else(COL_NOME == COL_NOME){
+    }else if (COL_NOME == COL_NOME){
         return m.getNome();
     }
+    return "";
     }
-    
+    public void setValueAt (Object aValue, int rowIndex, int columnIndex){
+        Marca m = linhas.get(rowIndex);
+        if (columnIndex == COL_ID){
+            m.setId((Integer)aValue);
+        }else if (columnIndex == COL_NOME){
+            m.setNome(aValue.toString());
+        }
+    }
+    //Indica se a celula é editável
+    public boolean isCellEditable (int rowIndex, int columnIndex){
+        return false; 
+    }
+    /*
+     * Retorna a marca referente a linha especificada
+     */
+    public Marca getMarca (int indiceLinha){
+        return linhas.get(indiceLinha);   
+    }
+    public void addMarca(Marca marca){
+        //Adiciona o registro
+        linhas.add(marca);
+        //Pega a quantidade de registros(Os registros começam com zero
+        int ultimoIndice = getRowCount() - 1;
+        
+        //Notifica a mudança
+        fireTableRowsInserted(ultimoIndice, ultimoIndice);
+        
+    }
+     public void udateMarca (int indiceLinha, Marca marca){
+         linhas.set(indiceLinha, marca);
+         fireTableRowsUpdated(indiceLinha, indiceLinha);
+         
+     }
+      
+     public void removeMarca (int indiceLinha){
+         linhas.remove(indiceLinha);
+         fireTableRowsDeleted (indiceLinha, indiceLinha);
+         
+     }
+   
 }

542e1947c18e09e51c0dd8f73a302de0c26c9a2f
guilherme_gustavo <guilherme_gustavo@10.1.48.192>
2013-08-02 14:53:31 -0300

    Criando a classe Marca.

---
+++ CrudJdbc/src/br/com/senai/negocio/Marca.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senai.negocio;
+
+/**
+ *
+ * @author guilherme_gustavo
+ */
+public class Marca {
+    int id;
+    String nome;
+    
+    public Marca(){
+        
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+}
